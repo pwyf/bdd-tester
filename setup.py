a861db27432a0f4362bf747ef1499d3e29cd7a57
Andy Lulham <a.lulham@gmail.com>
2017-05-23 14:26:14 +0100

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ from os.path import join
 from setuptools import setup
 
 
+requirements = """
+behave==1.2.5
+lxml==3.7.1
+requests
+"""
+
 setup(
     name='bdd-tester',
     scripts=[join('bin', 'bdd_tester')],
+    install_requires=requirements.strip().splitlines(),
 )
