94420b25b14e8aadc5c97a3852cb6691e3f90c7f
Andy Lulham <a.lulham@gmail.com>
2017-05-31 23:51:08 +0100

    Add some extra scenario context

--- environment.py
+++ environment.py
@@ -13,5 +13,10 @@ def before_scenario(context, scenario):
         # fetch the relevant activity and set it on the context
         activity = context.activities[row.index-1]
         context.xml = activity
+        try:
+            context.activity_id = activity.xpath('iati-identifier/text()')[0]
+        except:
+            context.activity_id = None
+        context.activity_idx = row.index
     else:
         context.xml = context.organisation
