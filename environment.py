8108cd9cc652577491df48ec71706e88d317e185
Andy Lulham <a.lulham@gmail.com>
2017-05-31 13:44:08 +0100

    Slightly neater way to get the current activity

--- environment.py
+++ environment.py
@@ -6,10 +6,8 @@ def before_scenario(context, scenario):
     if context.active_outline:
         # for scenario outlines, we set the relevant activity
         row = context.active_outline
-        # extract the activity number
-        activity_num = int(row.cells[0].split()[-1])
         # fetch the relevant activity and set it on the context
-        activity = context.activities[activity_num]
+        activity = context.activities[row.index-1]
         context.xml = activity
     else:
         context.xml = context.organisation
