43f33cc51e30f1b571529d66703131f8bdc986d2
Andy Lulham <a.lulham@gmail.com>
2017-06-02 23:33:24 +0100

    Set output_path default later

--- bdd_tester/__init__.py
+++ bdd_tester/__init__.py
@@ -4,6 +4,8 @@ from bdd_tester.runner import DQRunner
 
 
 def bdd_tester(filepath, **kwargs):
+    default_output_path = 'output'
+
     # we'll add the behave args to this list
     command_args = []
 
@@ -24,7 +26,9 @@ def bdd_tester(filepath, **kwargs):
     # pass stuff to behave via user-defined variables
     if kwargs.get('today'):
         command_args += ['--define', 'today=' + kwargs.get('today')]
-    command_args += ['--define', 'output_path=' + kwargs.get('output_path')]
+    if not kwargs.get('output_path'):
+        output_path = default_output_path
+    command_args += ['--define', 'output_path=' + output_path]
 
     # specify the location of the test files (features)
     command_args += kwargs.get('features')
