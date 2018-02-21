972e6bf52d1acbbb2db0ba297c5c4fb18791d406
Andy Lulham <a.lulham@gmail.com>
2018-02-21 22:44:30 +0000

    Exception should only fetch ID when it’s needed

--- bdd_tester/exceptions.py
+++ bdd_tester/exceptions.py
@@ -2,15 +2,23 @@ import json
 
 
 class StepException(Exception):
-    def __str__(self):
-        return self.msg
-
     def __init__(self, xml, msg=''):
         self.msg = msg
+        self.xml = xml
+
+    @property
+    def id(self):
         try:
-            self.id = xml.xpath('iati-identifier/text()')[0]
-        except:
-            self.id = ''
+            return self.id_
+        except AttributeError:
+            try:
+                self.id_ = self.xml.find('iati-identifier').text
+            except:
+                self.id_ = '[no-identifier]'
+        return self.id_
+
+    def __str__(self):
+        return '{}: {}'.format(self.id, self.msg)
 
     @property
     def json_output(self):
