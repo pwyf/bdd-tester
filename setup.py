a436378a72097b5402eca8f0ae1bbbac5cbfdfbb
Andy Lulham <a.lulham@gmail.com>
2019-01-28 23:21:34 +0000

    v0.0.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description='A very very basic BDD test runner',
     author='Andy Lulham',
     author_email='a.lulham@gmail.com',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     scripts=[join('bin', 'bdd_tester')],
     license='MIT',
